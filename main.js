ba0e69fbe112d2df6fe7abb171e20475fddf70ac
Ra Za <r.xitiz07@gmail.com>
2024-01-10 16:27:13 +0545

    changed

--- main.js
+++ main.js
@@ -7,25 +7,34 @@ const displaydirector = document.querySelector(".director");
 const displayawards = document.querySelector(".awards");
 const displayoverview = document.querySelector(".overview");
 const dispback = document.querySelector("body");
-const dispcountry = document.querySelector(".country")
-const disprating = document.querySelector(".rating")
+const dispcountry = document.querySelector(".country");
+const disprating = document.querySelector(".rating");
+
 async function fetchmovie() {
   const mname = movie.value;
-  try {
-    const out = await fetch(
-      `https://www.omdbapi.com/?i=tt3896198&apikey=81bd7c1e&t=${mname}`
-    );
-    const datas = await out.json();
-    console.log(datas);
-    displaytitle.innerHTML = `${datas.Title}`;
-    displayrealeseyear.innerHTML = `Released in ${datas.Released}`;
-    displayposter.src = `${datas.Poster}`;
-    displayGenre.innerHTML = `Genre: ${datas.Genre}`;
-    displaydirector.innerHTML = `Director: ${datas.Director}`;
-    displayawards.innerHTML = `Awards: ${datas.Awards}`;
-    displayoverview.innerHTML = `${datas.Plot}`;
-    dispback.style.backgroundImage = `url(${datas.Poster})`;
-    dispcountry.innerHTML = `Country: ${datas.Country}`
-    disprating.innerHTML = `Rating By ${datas.Ratings[0].Source}: ${datas.Ratings[0].Value}`
-  } catch {}
+  if (mname == "") {
+    displayposter.src = "icons/search.png";
+  } else {
+    try {
+      const out = await fetch(
+        `https://www.omdbapi.com/?i=tt3896198&apikey=81bd7c1e&t=${mname}`
+      );
+      const datas = await out.json();
+      if (datas.Error == "Movie not found!") {
+        displayposter.src = "icons/notfound.png";
+      } else {
+        console.log(datas);
+        displaytitle.innerHTML = `${datas.Title}`;
+        displayrealeseyear.innerHTML = `Released in ${datas.Released}`;
+        displayposter.src = `${datas.Poster}`;
+        displayGenre.innerHTML = `Genre: ${datas.Genre}`;
+        displaydirector.innerHTML = `Director: ${datas.Director}`;
+        displayawards.innerHTML = `Awards: ${datas.Awards}`;
+        displayoverview.innerHTML = `${datas.Plot}`;
+        dispcountry.innerHTML = `Country: ${datas.Country}`;
+        disprating.innerHTML = `Rating By ${datas.Ratings[0].Source}: ${datas.Ratings[0].Value}`;
+      }
+    } catch {
+    }
+  }
 }
